21971bf1935c1aa17ed197110d29d3889e8ec0c2
Nihaal Manesia <nihaal419@gmail.com>
2016-09-07 23:30:01 -0500

    Completed.

--- Flying with Naail Styles/ViewController.h
+++ Flying with Naail Styles/ViewController.h
@@ -8,6 +8,51 @@
 
 #import <UIKit/UIKit.h>
 
+int Y;
+BOOL Start;
+int RandomPosition;
+int ScoreNumber;
+int HighScore;
+
 @interface ViewController : UIViewController
+{
+    IBOutlet UILabel *Intro1;
+    IBOutlet UILabel *Intro2;
+    IBOutlet UILabel *Intro3;
+    IBOutlet UILabel *Intro4;
+    
+    IBOutlet UIImageView *Heli;
+    
+    NSTimer *timer;
+    
+    IBOutlet UIImageView *Obstacle;
+    IBOutlet UIImageView *Obstacle2;
+    IBOutlet UIImageView *Bottom1;
+    IBOutlet UIImageView *Bottom2;
+    IBOutlet UIImageView *Bottom3;
+    IBOutlet UIImageView *Bottom4;
+    IBOutlet UIImageView *Bottom5;
+    IBOutlet UIImageView *Bottom6;
+    IBOutlet UIImageView *Bottom7;
+    IBOutlet UIImageView *Bottom8;
+    IBOutlet UIImageView *Top1;
+    IBOutlet UIImageView *Top2;
+    IBOutlet UIImageView *Top3;
+    IBOutlet UIImageView *Top4;
+    IBOutlet UIImageView *Top5;
+    IBOutlet UIImageView *Top6;
+    IBOutlet UIImageView *Top7;
+    IBOutlet UIImageView *Top8;
+    
+    IBOutlet UILabel *Score;
+    
+    NSTimer *Scorer;
+}
+
+-(void)HeliMove;
+-(void)Collision;
+-(void)EndGame;
+-(void)NewGame;
+-(void)Scoring;
 
 @end
